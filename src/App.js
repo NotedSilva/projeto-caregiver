5d537e33e2da355a7f26d3bd08d9e4d0996c514e
nattan447 <nattanferreira58@gmail.com>
2023-10-09 20:04:31 -0300

    nattan

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from "./components/Navbar"
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+    //ola mundo
     <div className="App">
-      <Navbar/>
-
+      <Navbar />
     </div>
   );
 }
