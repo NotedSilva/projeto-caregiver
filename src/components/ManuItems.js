0e02788092dd9d321fd64b94f6cac213b0f691bb
Gabriel S. Jesus <gabriel_live12@outlook.com>
2023-10-08 21:44:32 -0300

    Primeiro commit, navbar finalizada

---
+++ src/components/ManuItems.js
@@ -0,0 +1,36 @@
+export const MenuItems = [
+    {
+        title : 'Home',
+        url : '#',
+        cName : 'nav-links',
+        icon : 'fa-solid fa-house-user'
+    },
+    {
+        title : 'Sobre Nós',
+        url : '#sobre-nos',
+        cName : 'nav-links',
+        icon : 'fa-solid fa-circle-info'
+    },
+    {
+        title : 'Serviços',
+        url : '#servicos',
+        cName : 'nav-links',
+        icon : 'fa-solid fa-briefcase'
+    },
+    {
+        title : 'Contato',
+        url : '#contato',
+        cName : 'nav-links',
+        icon : 'fa-solid fa-address-book'
+    },
+    {
+        title : 'Seja Cuidador',
+        url : '#cuidador',
+        cName : 'nav-links-mobile',
+    },
+    {
+        title : 'Conectar',
+        url : '#conectar',
+        cName : 'nav-links-mobile',
+    }
+]           
