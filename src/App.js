f7b8f003b6648fa7dcdc3e982307f7ebc06162b5
Gabriel S. Jesus <gabriel_live12@outlook.com>
2023-10-10 17:11:51 -0300

    inclusão do footer

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import Contato from './routes/Contato';
 
 function App() {
   return (
-    //ola mundo
     <div className="App">
       <Routes>
         <Route path="/" element={<Home/>}/> 
         <Route path="/sobrenos" element={<SobreNos/>}/> 
         <Route path="/Servicos" element={<Servicos/>}/> 
-        <Route path="/Contato" element={<Contato/>}/> 
+        <Route path="/Contato" element={<Contato/>}/>
       </Routes>
     </div>
   );
