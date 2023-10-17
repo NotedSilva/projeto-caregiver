a18f6c2b48debad527837bb3a3426a4324682a79
nattan447 <nattanferreira58@gmail.com>
2023-10-16 21:29:54 -0300

    adicionei a pasta contextos para futuro uso

--- src/components/ContatoForm.js
+++ src/components/ContatoForm.js
@@ -1,31 +1,56 @@
-import './ContatoFormStyles.css'
-import img8 from '../assets/8.svg'
+import "./ContatoFormStyles.css";
+import img8 from "../assets/8.svg";
+import { useEffect, useState, useContext } from "react";
 
-function ContatoForm(){
-    return(
-        <div className="Contato-Form">
-            <h1>Contate-nos</h1>
-            <div className="ctn">
-                <div className="ctn-main">
-                    <div className="ctn-content">
-                        <h2>Contato</h2>
-                        <form action="#" method="post">
-                            <input type="text" name="name" placeholder='Insira seu Nome'></input>
+function ContatoForm() {
+  //testando pegar dados do input
+  const [email, SetEamil] = useState("");
+  const handleemail = (txt) => {
+    SetEamil(txt.target.value);
+  };
+  function seeemail() {
+    alert(email);
+  }
 
-                            <input type="email" name="name" placeholder='Insira seu E-mail'></input>
+  return (
+    <div className="Contato-Form">
+      <h1>Contate-nos</h1>
+      <div className="ctn">
+        <div className="ctn-main">
+          <div className="ctn-content">
+            <h2>Contato</h2>
+            <form action="#" method="post">
+              <input
+                type="text"
+                name="name"
+                placeholder="Insira seu Nome"
+              ></input>
 
-                            <textarea name="message" placeholder="Insira sua Mensagem"></textarea>
+              <input
+                type="email"
+                name="name"
+                placeholder="Insira seu E-mail"
+                value={email}
+                onChange={handleemail}
+              ></input>
 
-                            <button type="submit" className="btn">Enviar <i className="fas fa-paper-plane"></i></button>
-                        </form>
-                    </div>
-                    <div className="form-img">
-                        <img src={img8} alt="Contato"></img>
-                    </div>
-                </div>
-            </div>
+              <textarea
+                name="message"
+                placeholder="Insira sua Mensagem"
+              ></textarea>
+
+              <button type="submit" className="btn" onClick={seeemail}>
+                Enviar <i className="fas fa-paper-plane"></i>
+              </button>
+            </form>
+          </div>
+          <div className="form-img">
+            <img src={img8} alt="Contato"></img>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default ContatoForm;
