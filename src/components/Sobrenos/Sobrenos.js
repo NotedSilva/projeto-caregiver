7655a0061d01a7ee48584e1d898702461258f288
Gabriel S. Jesus <gabriel_live12@outlook.com>
2023-10-24 17:51:53 -0300

    Ajustes

--- src/components/Sobrenos/Sobrenos.js
+++ src/components/Sobrenos/Sobrenos.js
@@ -1,55 +1,28 @@
+import React from 'react';
+import {Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel, AccordionItemState} from 'react-accessible-accordion';
+import "react-accessible-accordion/dist/fancy-example.css";
+import {MdOutlineArrowDrop} from 'react-icons/md';
 import '../Sobrenos/SobrenosStyles.css';
+import img15 from '../../assets/5.jpg'
 
-function Sobrenos() {
-  //pego a variável que passei no arquivo app
-  /*const { nomeUser } = useContext(Logincontexto);*/
+const Sobrenos = () => {
   return (
-    <div className="Sobrenos-container">
-      <h1>
-        {" "}
-        <i class="fa-solid fa-circle-check"></i> Missão
-      </h1>
-      <p>
-        Nossa missão é cuidar de forma compassiva e profissional das pessoas e
-        animais de estimação que confiam em nós. Queremos tornar a vida de
-        nossos clientes mais fácil e mais feliz, proporcionando-lhes um ambiente
-        seguro e amoroso.
-      </p>
+    <section className="v-wrapper">
+      <div className="paddings innerWidht flexCenter v-container">
+        <div className="v-left">
+          <div className="image-container">
+            <img src={img15} alt='Imagem' />
+          </div>
+        </div>
 
-      <h1>
-        <i class="fa-solid fa-heart"></i> Visão
-      </h1>
-      <p>
-        A Caregiver nasceu em 2005 com um objetivo claro: proporcionar cuidado
-        de qualidade para idosos, crianças e animais de estimação. Acreditamos
-        que o cuidado atencioso é a base para uma vida feliz e saudável, e é
-        isso que nos motiva todos os dias.
-      </p>
+        <div className="flexColStart v-right">
+          <span className="orangeText">Nossos valores</span>
+          <span>Valores da nossa equipe</span>
+        </div>
 
-      <h1>
-        <i class="fa-solid fa-eye"></i> Valores
-      </h1>
-      <p>
-        Compaixão: O cuidado é a essência de tudo o que fazemos. Demonstramos
-        compaixão em todas as interações com nossos clientes e seus entes
-        queridos.
-      </p>
-      <p>
-        Profissionalismo: Mantemos os mais altos padrões de profissionalismo em
-        todos os nossos serviços, garantindo que nossos clientes possam confiar
-        em nós.
-      </p>
-      <p>
-        Segurança e Bem-Estar: A segurança e o bem-estar de nossos clientes,
-        sejam eles idosos, crianças ou pets, são nossa prioridade número um.
-      </p>
-      <p>
-        Crescimento Contínuo: Buscamos constantemente aprimorar nossos serviços
-        e expandir para atender mais comunidades, levando o cuidado da Caregiver
-        a um público cada vez maior.
-      </p>
-    </div>
-  );
+      </div>
+    </section>
+  )
 }
 
-export default Sobrenos;
+export default Sobrenos
---
+++ src/components/Sobrenos/accordion.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {HiShieldCheckbox} from 'react-icons/hi';
+import {MdCancel, MdAnalytics} from 'react-icons/md';
+
+const DbAccordion = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best saller in the market",
+    detail: "lorem psum aksjasaslasokaojksajkosjoasjajisjiasjiasji"
+  },
+  {
+    icon: <MdCancel/>,
+    heading: "Best saller in the market",
+    detail: "lorem psum aksjasaslasokaojksajkosjoasjajisjiasjiasji"
+  },
+  {
+    icon: <MdAnalytics/>,
+    heading: "Best saller in the market",
+    detail: "lorem psum aksjasaslasokaojksajkosjoasjajisjiasjiasji"
+  },
+  
+] 
+
+
+export default DbAccordion;
