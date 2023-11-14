45b17de12ae0d40812fbf6e8f78d5d0d62a2cf1d
Gabriel S. Jesus <gabriel_live12@outlook.com>
2023-11-14 12:54:42 -0300

    Ajustes de estilos

--- src/routes/Conectar.js
+++ src/routes/Conectar.js
@@ -7,7 +7,7 @@ const Conectar=()=>{
 return( 
 <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundImage: `url(${img1010})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
         <Navbar />
-    <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+    <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '50rem' }}>
     <Login/>
     </div>
     <Footer />
