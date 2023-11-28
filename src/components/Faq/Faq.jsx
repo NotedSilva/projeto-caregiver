2b8887274c9b0ea9024317e118eef342ca070fe0
Gabriel S. Jesus <gabriel_live12@outlook.com>
2023-11-28 19:49:26 -0300

    Ajustes

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -3,23 +3,23 @@ import '../Faq/FaqStyles.css';
 
 const dataFaq = [
   {
-    question: '1. Como posso me cadastrar como cuidador na plataforma "Caregiver"?',
+    question: 'Como posso me cadastrar como cuidador na plataforma "Caregiver"?',
     answer: 'Para se cadastrar como cuidador na plataforma "Caregiver", siga os seguintes passos:\n - Acesse o site ou aplicativo "Caregiver".\n - Clique em "Cadastro" ou "Registrar".\n - Preencha o formulário de inscrição com suas informações pessoais, experiência, habilidades e certificações.\n - Aguarde a aprovação da sua inscrição pela equipe "Caregiver".'
   },
   {
-    question: '2. Sou um usuário em busca de um cuidador. Como posso encontrar um cuidador adequado?',
+    question: 'Sou um usuário em busca de um cuidador. Como posso encontrar um cuidador adequado?',
     answer: 'Para encontrar um cuidador adequado na plataforma "Caregiver", siga os seguintes passos:\n - Faça login na sua conta ou cadastre-se como usuário.\n - Navegue pelos perfis de cuidadores disponíveis.\n - Filtrar os cuidadores com base em suas necessidades, como localização, disponibilidade e especializações.\n - Analise os perfis dos cuidadores, suas avaliações e experiências anteriores.\n - Inicie uma conversa com um cuidador e prossiga com a contratação se estiver satisfeito.'
   },
   {
-    question: '3. Como funciona o sistema de pagamento na plataforma "Caregiver"?',
+    question: 'Como funciona o sistema de pagamento na plataforma "Caregiver"?',
     answer: 'A plataforma "Caregiver" oferece um sistema de pagamento integrado. Os detalhes do pagamento podem variar, mas geralmente, o pagamento é processado de forma segura e transparente através da plataforma. Os cuidadores podem receber pagamentos diretamente na plataforma após a conclusão dos serviços.'
   },
   {
-    question: '4. Como são feitas as avaliações e feedbacks?',
+    question: 'Como são feitas as avaliações e feedbacks?',
     answer: 'As avaliações e feedbacks são uma parte essencial da plataforma "Caregiver". Após a conclusão de um serviço, tanto o usuário quanto o cuidador podem avaliar um ao outro. Isso ajuda a criar um ambiente confiável e permite que outros usuários tomem decisões informadas ao escolher um cuidador ou um cliente.'
   },
   {
-    question: '5. Existe um sistema de chat para comunicação entre cuidadores e usuários?',
+    question: 'Existe um sistema de chat para comunicação entre cuidadores e usuários?',
     answer: 'Sim, a plataforma "Caregiver" fornece um sistema de chat integrado que permite a comunicação direta entre cuidadores e usuários. Você pode usar o chat para discutir detalhes do serviço, disponibilidade, requisitos e outras informações relevantes.'
   }
 ];
